bd603c5831cdfcdb3173ab3f6bd45d8349f4ac72
Stefan Arn <arnstefan@gmail.com>
2015-10-16 12:21:29 +0200

    some todos added

--- classicFilms/classicFilms/AppDelegate.m
+++ classicFilms/classicFilms/AppDelegate.m
@@ -37,6 +37,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    
+    
     [[ARNArchiveController sharedInstance] fetchMovieArchiveForCollection:@"feature_films"];
     //[[ARNArchiveController sharedInstance] fetchMovieArchiveForCollection:@"SciFi_Horror"];
 }
--- classicFilms/classicFilms/data-controllers/ARNMovieDBController.m
+++ classicFilms/classicFilms/data-controllers/ARNMovieDBController.m
@@ -70,7 +70,28 @@
            
         } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
             NSLog(@"Error: %@", error);
+            
+            // TODO: this api has a rate limit - uuugh
+            // TODO stop the current for loop
+            // wait for Retry-after as seconds
+            // restart this method with all movie objects with have a refresh date older than 10 minutes
+            
+            /*
+             Error: Error Domain=com.alamofire.error.serialization.response Code=-1011 "Request failed: client error (429)" UserInfo={com.alamofire.serialization.response.error.response=<NSHTTPURLResponse: 0x7fc31ae59d60> { URL: http://api.themoviedb.org/3/search/movie?api_key=cde3935be83a0ceff90f530f19931df3&query=Charlie%20Chaplin%27s%20%22The%20Cure%22&year=0 } { status code: 429, headers {
+             "Access-Control-Allow-Origin" = "*";
+             Connection = "keep-alive";
+             "Content-Length" = 95;
+             "Content-Type" = "application/json; charset=utf-8";
+             Date = "Fri, 16 Oct 2015 09:53:58 GMT";
+             "Retry-After" = 8;
+             Server = openresty;
+             } }, NSErrorFailingURLKey=http://api.themoviedb.org/3/search/movie?api_key=cde3935be83a0ceff90f530f19931df3&query=Charlie%20Chaplin%27s%20%22The%20Cure%22&year=0, com.alamofire.serialization.response.error.data=<7b227374 61747573 5f636f64 65223a32 352c2273 74617475 735f6d65 73736167 65223a22 596f7572 20726571 75657374 20636f75 6e742028 34312920 6973206f 76657220 74686520 616c6c6f 77656420 6c696d69 74206f66 2034302e 227d0a>, NSLocalizedDescription=Request failed: client error (429)}
+             
+             */
         }];
+        
+        
+        // TODO: we have to limit the for loop refresh to the rate limit of the api: We currently rate limit requests to 30 requests every 10 seconds.
     }
 }
 
