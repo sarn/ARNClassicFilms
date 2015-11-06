1f3e494fd54a8a58b3d3f423b90fa05fab897bdc
Stefan Arn <arnstefan@gmail.com>
2015-11-06 11:47:51 +0100

    better real world poster spacing and sizing

--- classicFilms/classicFilms/view-controllers/ARNMovieOverviewController.m
+++ classicFilms/classicFilms/view-controllers/ARNMovieOverviewController.m
@@ -209,19 +209,19 @@
 #pragma mark UICollectionViewDelegateFlowLayout methods
 
 -(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    return UIEdgeInsetsMake(100.0f, 50.0f, 100.0f, 50.0f);
+    return UIEdgeInsetsMake(100.0f, 80.0f, 100.0f, 80.0f);
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
-    return 50.0f;
+    return 20.0f;
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
-    return 40.0f;
+    return 20.0f;
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return CGSizeMake(400, 850);
+    return CGSizeMake(256, 544);
 }
 
 
